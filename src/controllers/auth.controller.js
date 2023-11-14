28c82ea116e3655784d135f40e77cbf0f249260d
Lucas Lopez <lucaslopezmaxi7@gmail.com>
2023-11-14 00:56:40 -0300

    task api version 1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,35 +1,74 @@
 import { pool } from '../db.js'
 import bcrypt from 'bcrypt'
+import { createAccessToken } from '../libs/jwt.js'
+import md5 from 'md5'
 
-export const signin = (req, res) => {
-  res.send('ingresando')
+export const signin = async (req, res) => {
+  const { email, password } = req.body
+
+  const result = await pool.query('SELECT * FROM users WHERE email = $1', [
+    email
+  ])
+  if (!result.rowCount) {
+    return res.status(400).json({
+      message: 'El correo no esta registrado'
+    })
+  }
+
+  const validPassword = await bcrypt.compare(password, result.rows[0].password)
+
+  if (!validPassword) {
+    return res.status(400).json({ message: 'La contraseña es incorrecta.' })
+  }
+
+  const token = await createAccessToken({ id: result.rows[0].id })
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    //secure: true,
+    sameSite: 'none',
+    maxAge: 24 * 60 * 60 * 1000 // 1 day
+  })
+  return res.json(result.rows[0])
 }
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { name, email, password } = req.body
   try {
-
     const hashedPassword = await bcrypt.hash(password, 10)
+    const gravatar = `https://www.gravatar.com/avatar/${md5(email)}`;
 
-    console.log(hashedPassword);
-    const results = await pool.query(
-      'INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *',
-      [name, email, hashedPassword]
+    const result = await pool.query(
+      'INSERT INTO users(name, email, password, gravatar) VALUES($1, $2, $3, $4) RETURNING *',
+      [name, email, hashedPassword, gravatar]
     )
-    console.log(results)
-    return res.status(201).json(results.rows[0])
+
+    const token = await createAccessToken({ id: result.rows[0].id })
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      //secure: true,
+      sameSite: 'none',
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    })
+    return res.json(result.rows[0])
+    // return res.status(201).json(results.rows[0])
   } catch (error) {
-    if(error.code === '23505') {
-      return res.status(400).json({message: 'El correo ya esta registrado'})
+    if (error.code === '23505') {
+      return res.status(400).json({ message: 'El correo ya esta registrado' })
     }
+
+    next(error)
   }
 }
 
 export const logout = (req, res) => {
-  const { id } = req.params
-  res.send('cerrando sesion')
+  res.clearCookie('token')
+  res.sendStatus(200)
 }
 
-export const profile = (req, res) => {
-  res.send('perfil del usuario')
+export const profile = async (req, res) => {
+  const result = await pool.query('SELECT * FROM users WHERE id = $1', [req.userId])
+
+  return res.status(200).json(result.rows[0])
 }
--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,15 +1,16 @@
 import { pool } from '../db.js'
 
-export const getAllTasks = async (req, res, next) => {
-  const results = await pool.query('SELECT * FROM task')
+export const getAllTasks = async (req, res) => {
+  const results = await pool.query('SELECT * FROM task WHERE user_id = $1', [req.userId])
   res.json(results.rows)
 }
 
 export const getTask = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
+
   const result = await pool.query('SELECT * FROM task WHERE id = $1', [id])
-  if(!result.rowCount) {
-    return res.status(404).json({message:'La tarea no existe'})
+  if (!result.rowCount) {
+    return res.status(404).json({ message: 'La tarea no existe' })
   }
 
   return res.json(result.rows[0])
@@ -20,38 +21,45 @@ export const createTask = async (req, res, next) => {
 
   try {
     const result = await pool.query(
-      'INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *',
-      [title, description]
+      'INSERT INTO task (title, description, user_id) VALUES ($1, $2, $3) RETURNING *',
+      [title, description, req.userId]
     )
     res.json(result.rows[0])
   } catch (error) {
     if (error.code === '23505') {
-      return res.status(409).json({message: 'Ya existe una tarea con ese titulo'})
+      return res
+        .status(409)
+        .json({ message: 'Ya existe una tarea con ese titulo' })
     }
     next(error)
   }
 }
 
 export const updateTask = async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
+  const { id } = req.params
+  const { title, description } = req.body
 
-  const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *',
-  [title, description, id])
-  if(!result.rowCount) {
-    return res.status(404).json({message: 'La tarea no existe'})
+  const result = await pool.query(
+    'UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *',
+    [title, description, id]
+  )
+  if (!result.rowCount) {
+    return res.status(404).json({ message: 'La tarea no existe' })
   }
-  console.log(result);
+  console.log(result)
   return res.json(result.rows[0])
 }
 
 export const deleteTask = async (req, res) => {
   const { id } = req.params
-  const result = await pool.query('DELETE FROM task WHERE id = $1 RETURNING *', [id])
-  console.log(result);
-  
-  if(!result.rowCount) {
-    return res.status(404).json({message: 'La tarea no existe'})
+  const result = await pool.query(
+    'DELETE FROM task WHERE id = $1 RETURNING *',
+    [id]
+  )
+  console.log(result)
+
+  if (!result.rowCount) {
+    return res.status(404).json({ message: 'La tarea no existe' })
   }
 
   return res.sendStatus(204)
